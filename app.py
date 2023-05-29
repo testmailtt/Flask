0288673f8d0f820564f9167e4279d69590e953b4
testmailtt <74116260+testmailtt@users.noreply.github.com>
2023-05-29 08:40:49 +0100

    sr

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pydub import AudioSegment
 import youtube_dl
+import speech_recognition as sr
 
 app = Flask(__name__)
 Base = declarative_base()
